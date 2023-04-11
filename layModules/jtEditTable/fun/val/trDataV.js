623b78ddb4e8c4015d6c6406330a8f299dda07ab
何波 <tbydmi@gmail.com>
2023-04-11 10:31:03 +0800

    Update trDataV.js

--- layModules/jtEditTable/fun/val/trDataV.js
+++ layModules/jtEditTable/fun/val/trDataV.js
@@ -1,7 +1,7 @@
-import { data } from "../../var/index";
+import { data, def_data_tr } from "../../var/index";
 
 export function trDataV(i, key, v) {
-  let trData = data[i]
+  let trData = data[i] || def_data_tr
   if (key === undefined) {
     if (v === undefined) {
       return trData
