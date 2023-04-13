917edbcab0ea53575828c17d846e045358bc5398
何波 <tbydmi@gmail.com>
2023-04-13 11:39:50 +0800

    Update eventRow.js

--- layModules/jtEditTable/fun/event/eventRow.js
+++ layModules/jtEditTable/fun/event/eventRow.js
@@ -1,24 +1,26 @@
 import { elem_p, isInit, rowCheck, rowClick, selected_tr, skinSelected, skin_selected } from "../../var/index";
+import { getInputElems } from "../other/getElem";
 import { getRowData } from "../prop/getRowData";
-import { trDataV } from "../val/trDataV";
-
+import { judgeAddRow } from "../reload/addRow";
 export function eventClickRow(res) {
   if (isInit) {
     let i = selected_tr ? selected_tr.index : 0
     elem_p.find(skinSelected).removeClass(skin_selected)
     res.tr.addClass(skin_selected)
     selected_tr = res
-    if (rowClick) {
-      getRowData().then(rowClick)
-    }
     if (i !== res.index && rowCheck) {
       getRowData().then(rowCheck)
     }
+    if (rowClick) {
+      getRowData().then(rowClick)
+    }
   }
 }
-export function eventEditRow(res) {
-  if (isInit) {
-    let key = res.field, i = res.tr.attr('data-index'), v = res.value
-    trDataV(i, key, v)
+export function evnetKeyupLastRow(e) {
+  if (isInit && e.keyCode === 13) {
+    let tr = $(e.target).parents('tr'), lastElem = getInputElems(tr).last()[0]
+    if (e.target == lastElem || e.target.parentElement == lastElem) {
+      judgeAddRow()
+    }
   }
 }
