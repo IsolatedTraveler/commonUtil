f4cccc15ea863254cba6504b016c15975a0cac5d
何波 <tbydmi@gmail.com>
2023-04-13 13:51:03 +0800

    Update initEvent.js

--- layModules/jtEditTable/fun/init/initEvent.js
+++ layModules/jtEditTable/fun/init/initEvent.js
@@ -1,16 +1,16 @@
 import { elem_f, elem_p, enterAdd, name, third_table, zb_filter } from "../../var/index";
-import { colKeyup, eventEditCol, evnetKeyupLastRow } from "../event/eventCol";
-import { eventEditRow, eventClickRow } from "../event/eventRow";
-import { eventEidtInputCol } from "../event/eventInputCol";
+import { colKeyup, eventEditColTd, eventEditInputCol } from "../event/eventCol";
+import { eventClickRow, evnetKeyupLastRow } from "../event/eventRow";
+import { eventZbEditInputCol } from "../event/eventZb";
 export function initEvent() {
   third_table.on(`row(${name})`, eventClickRow)
-  third_table.on(`edit(${name})`, eventEditRow)
-  elem_p.on('change', '[name]:not([laydate])', eventEditCol)
+  third_table.on(`edit(${name})`, eventEditColTd)
+  elem_p.on('change', '[name]:not([laydate])', eventEditInputCol)
   elem_p.on('keyup', commonUtil.kbjbg, colKeyup)
   if (enterAdd) {
     elem_p.on('keyup', '>.layui-table-view>.layui-table-box>.layui-table-body>table>tbody>tr:last', evnetKeyupLastRow)
   }
   if (elem_f && elem_f[0]) {
-    elem_f.on('change', zb_filter, eventEidtInputCol)
+    elem_f.on('change', zb_filter, eventZbEditInputCol)
   }
 }
