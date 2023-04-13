3c24025e7eeb4b410b6f6f072db065105864498b
何波 <tbydmi@gmail.com>
2023-04-13 13:59:48 +0800

    Create tableLoaded.js

---
+++ layModules/jtEditTable/fun/initReload/tableLoaded.js
@@ -0,0 +1,11 @@
+import { autoHeight, done, elem_t_s, elem_t_v } from "../../var/index"
+import { endRender } from "../other/changeIsInit"
+export function tableLoaded() {
+  if (autoHeight) {
+    let auto = {height: 'auto'}
+    elem_t_v.css(auto)
+    elem_t_s.css(auto)
+  }
+  done && done()
+  endRender()
+}
