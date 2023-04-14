e4cd8924146e597509fb16891e8c552bf917723f
何波 <tbydmi@gmail.com>
2023-04-14 09:07:33 +0800

    Update initEvent.js

--- layModules/jtEditTable/fun/init/initEvent.js
+++ layModules/jtEditTable/fun/init/initEvent.js
@@ -1,4 +1,4 @@
-import { elem_f, elem_p, enterAdd, name, third_table, zb_filter } from "../../var/index";
+import { elem_f, elem_p, enterAdd, name, third_table, zb_change_cols, zb_filter } from "../../var/index";
 import { colKeyup, eventEditColTd, eventEditInputCol } from "../event/eventCol";
 import { eventClickRow, evnetKeyupLastRow } from "../event/eventRow";
 import { eventZbEditInputCol } from "../event/eventZb";
@@ -10,7 +10,7 @@ export function initEvent() {
   if (enterAdd) {
     elem_p.on('keyup', '>.layui-table-view>.layui-table-box>.layui-table-body>table>tbody>tr:last', evnetKeyupLastRow)
   }
-  if (elem_f && elem_f[0]) {
+  if (elem_f && elem_f[0] && zb_change_cols) {
     elem_f.on('change', zb_filter, eventZbEditInputCol)
   }
 }
