52b8f8935a366b3012cc8308966404413a944371
何波 <tbydmi@gmail.com>
2023-04-14 09:07:29 +0800

    Update eventRow.js

--- layModules/jtEditTable/fun/event/eventRow.js
+++ layModules/jtEditTable/fun/event/eventRow.js
@@ -1,7 +1,7 @@
 import { elem_p, isInit, rowCheck, rowClick, selected_tr, skinSelected, skin_selected } from "../../var/index";
 import { getInputElems } from "../other/getElem";
 import { getRowData } from "../prop/getRowData";
-import { judgeAddRow } from "../reload/addRow";
+import { judgeAdd } from "../reload/addRow";
 export function eventClickRow(res) {
   if (isInit) {
     let i = selected_tr ? selected_tr.index : 0
@@ -20,7 +20,7 @@ export function evnetKeyupLastRow(e) {
   if (isInit && e.keyCode === 13) {
     let tr = $(e.target).parents('tr'), lastElem = getInputElems(tr).last()[0]
     if (e.target == lastElem || e.target.parentElement == lastElem) {
-      judgeAddRow()
+      judgeAdd([{}])
     }
   }
 }
