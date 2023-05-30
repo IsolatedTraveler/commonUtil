ec408021736d8a9f9467cab02b5319155db1a6f4
何波 <tbydmi@gmail.com>
2023-05-30 15:48:38 +0800

    Delete old.js

--- build/task/old.js
+++
@@ -1,4 +0,0 @@
-const {getCode} = require('../fun/public'), {ml, outMl} = require('../var/old')
-module.exports = async function(grunt, version) {
-  await getCode('old.js', ml, version, grunt, outMl)
-}
