4531b76d164754880709553f3779914586199410
何波 <tbydmi@gmail.com>
2023-05-30 16:13:34 +0800

    tongbu

--- Gruntfile.js
+++ Gruntfile.js
@@ -16,7 +16,7 @@ module.exports = function(grunt) {
       dev: {
         src: [
           "src/",
-          // "global/",
+          "global/",
           "layModules/",
           'modules/'
         ]
