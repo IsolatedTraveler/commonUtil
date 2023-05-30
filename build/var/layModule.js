0fc088706c8cb9276fc2d1baac7bf91b2bb1ae06
何波 <tbydmi@gmail.com>
2023-05-30 15:49:21 +0800

    Update layModule.js

--- build/var/layModule.js
+++ build/var/layModule.js
@@ -1,4 +1,4 @@
-const path = require('path'), {ml, date, out} = require('./public'), name = 'layModules'
+const path = require('path'), { ml, date, out } = require('./public'), name = 'layModules'
 module.exports = {
   ml: path.resolve(ml, name),
   outMl: path.resolve(out, 'layui-v2.5.7/extend') + '\\',
