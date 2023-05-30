392667f14c97138bfd179bc20082cda7a4336b23
何波 <tbydmi@gmail.com>
2023-05-30 16:11:23 +0800

    移除

--- src/old/xzqh.js
+++
@@ -1,9 +0,0 @@
-import { that } from "../var/init"
-
-export function xzqh(id) {
-  var data = that.commonHttppost('/zs03-ywzd/xzqhgl/s-xzqh', {id}).data
-  return (data ? data[0] : data) || {}
-}
-export function zxzqhget(sjid) {
-  return that.commonHttppost('/zs03-ywzd/xzqhgl/s-xzqh', {sjid}).data
-}
