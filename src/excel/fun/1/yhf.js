3f0a099f15be695488d6443ffc5a3ce5f20a6314
何波 <tbydmi@gmail.com>
2023-06-05 16:07:11 +0800

    同步

--- src/excel/fun/1/yhf.js
+++ src/excel/fun/1/yhf.js
@@ -6,23 +6,19 @@ function getYhfStr(lx) {
     res = 'OR('
   } else if (lx === '!') {
     res = 'NOT('
-  } else if (lx === 'null') {
-    res = 'ISBLANK('
   } else {
-    console.error(`未设置【${lx}】该类型的处理方案`)
+    res = 'ISBLANK('
   }
   return res
 }
 export function yhf(lx, tj) {
   var res = getYhfStr(lx)
   if (Array.isArray(tj) && tj.length) {
-    tj = tj.map((it) => {
+    tj = tj.filter(it => it).map((it) => {
       if (typeof it === 'string') {
         return it
-      } else if (it.lx && it.val) {
-        return yhf(it.lx, it.val.map(v => {
-          return yhf(v[0], v[1])
-        }))
+      } else if (it.val) {
+        return yhf(it.lx, it.val)
       }
     }).join(',')
   } else {
