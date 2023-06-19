abc30863ecde84e93635a0b3a1b563093a5a018f
何波 <tbydmi@gmail.com>
2023-06-19 14:35:20 +0800

    Update layui.js

--- src/commonUtil/var/layui.js
+++ src/commonUtil/var/layui.js
@@ -1,4 +1,6 @@
-export let TBox = '.layui-table-view>.layui-table-box', TMain = '>.layui-table-body,>.layui-table-fixed>.layui-table-body', TBody = '>table>tbody'
+export let TBox = '.layui-table-view>.layui-table-box',
+  TMain = '>.layui-table-body,>.layui-table-fixed>.layui-table-body',
+  TBody = '>table>tbody'
 export default {
   TBox,
   TMain,
