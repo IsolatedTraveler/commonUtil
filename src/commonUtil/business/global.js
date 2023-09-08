3fea296a7f879fe30a91295b67dc014f922188a3
何波 <tbydmi@gmail.com>
2023-09-08 17:17:36 +0800

    Update global.js

--- src/commonUtil/business/global.js
+++ src/commonUtil/business/global.js
@@ -1,4 +1,6 @@
+import { load } from "../../../global/layer/public/load";
 import { readXlsx } from "../../../global/xlsx/public/xlsxTotable";
 import Class from "../core";
 
 Class.prototype.readXlsx = readXlsx
+Class.prototype.load = load
