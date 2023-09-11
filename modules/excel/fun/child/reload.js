a3fabd27460c84308b151a26487612bacede3067
何波 <tbydmi@gmail.com>
2023-09-11 17:09:06 +0800

    Update reload.js

--- modules/excel/fun/child/reload.js
+++ modules/excel/fun/child/reload.js
@@ -1,9 +1,14 @@
 // eslint-disable-next-line no-unused-vars
-import { cols, fbData, rowKeys, rows, zbData } from "../../var/index";
-export function reload() {
+import { cols, czlx, fbData, rowKeys, rows, zbData } from "../../var/index";
+import { setCol } from "./child";
+export function reload(config) {
   rowKeys = {}
   rows = []
   cols[0] = rows
   zbData = []
   fbData = []
+  czlx = config.czlx || 'hbbg'
+  setCol('文件名')
+  setCol('表名')
+  setCol('表序号')
 }
