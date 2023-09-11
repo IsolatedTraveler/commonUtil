493ae002d40661f9ef6be7e122d4f02f0b4bff3e
何波 <tbydmi@gmail.com>
2023-09-11 10:54:23 +0800

    同步

--- build/task/layModules.js
+++ build/task/layModules.js
@@ -1,9 +1,9 @@
-const {getCode, readDir} = require('../fun/public'), {ml, outMl} = require('../var/layModule'), path = require('path')
-module.exports = async function(grunt, version) {
+const { getCode, readDir } = require('../fun/public'), { ml, outMl } = require('../var/layModule'), path = require('path')
+module.exports = async function (grunt, version) {
   let res = await readDir(ml)
   if (res && res[0]) {
     let len = res.length
-    for(let i = 0; i < len; i++) {
+    for (let i = 0; i < len; i++) {
       let name = res[i]
       await getCode(name + '.js', path.resolve(ml, name), version, grunt, outMl)
     }
