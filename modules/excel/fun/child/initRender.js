00728c1849df5ef70c8ff46c543a7597ee1aed00
何波 <tbydmi@gmail.com>
2023-09-11 16:54:18 +0800

    tb

--- modules/excel/fun/child/initRender.js
+++ modules/excel/fun/child/initRender.js
@@ -1,5 +1,8 @@
-import { getInputFile, reload } from "./child/index";
+import { getInputFile, reload, setCol } from "./child/index";
 export function initRender() {
   getInputFile()
   reload()
+  setCol('文件名')
+  setCol('表名')
+  setCol('表序号')
 }
--- modules/excel/public/index.js
+++ modules/excel/public/index.js
@@ -1,2 +1,1 @@
-export * from './readXlsx'
 export * from './hbbg'
