54cea8c3d0ea429f1d3f2630b90b08856d754f9d
何波 <tbydmi@gmail.com>
2023-09-11 16:36:11 +0800

    Update setCol.js

--- modules/excel/fun/child/child/setCol.js
+++ modules/excel/fun/child/child/setCol.js
@@ -1,6 +1,8 @@
 import { rowKeys, rows } from "../../../var/global";
 
 export function setCol(field = '') {
-  rowKeys[field] = true
-  rows.push({ field, title: field, wdth: (field.length + 1) * 15 })
+  if (!rowKeys[field]) {
+    rowKeys[field] = true
+    rows.push({ field, title: field, wdth: (field.length + 1) * 15 })
+  }
 }
