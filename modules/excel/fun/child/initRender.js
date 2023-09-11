78b5145f58b3d03f91b83c0436a94881e44166ba
何波 <tbydmi@gmail.com>
2023-09-11 16:36:14 +0800

    Update initRender.js

--- modules/excel/fun/child/initRender.js
+++ modules/excel/fun/child/initRender.js
@@ -1,5 +1,9 @@
+import { cols, rowKeys, rows } from "../../var";
 import { getInputFile } from "./child/index";
 
 export function initRender() {
+  rowKeys = {}
+  rows = []
+  cols[0] = rows
   getInputFile()
 }
