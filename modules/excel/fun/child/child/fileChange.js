dad0e95e1afc16ec6b43fa492833f8fda6f74d1a
何波 <tbydmi@gmail.com>
2023-09-11 17:13:24 +0800

    Update fileChange.js

--- modules/excel/fun/child/child/fileChange.js
+++ modules/excel/fun/child/child/fileChange.js
@@ -1,7 +1,9 @@
 import { layerIndex } from "../../../var/global"
 
 export function fileChange(e) {
-  return Promise.all([].map.call(e.target.files, (it) => it)).then(() => { }).finally(() => {
+  return Promise.all([].map.call(e.target.files, (it) => {
+    console.log(it.name)
+  })).then(() => { }).finally(() => {
     layer.close(layerIndex)
   })
 }
