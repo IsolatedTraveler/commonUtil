54c930613588a2ce9505053a37c0cf75564be4ca
何波 <tbydmi@gmail.com>
2023-09-12 09:54:14 +0800

    Update hbbg.js

--- modules/excel/public/hbbg.js
+++ modules/excel/public/hbbg.js
@@ -1,6 +1,11 @@
+import { fbData, zbDataObj } from "../var";
 
 // 合并表格
 export function hbbg() {
+  console.log(zbDataObj)
+  console.log(fbData[0])
+  fbData = fbData.filter(it => {
+  })
 }
 // 对比合并表格
 export function dbhbbg() {
