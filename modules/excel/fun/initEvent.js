bc60acf8975a8e1438fdef52f5b7f9a9fd418b6f
何波 <tbydmi@gmail.com>
2023-09-12 10:06:05 +0800

    Update initEvent.js

--- modules/excel/fun/initEvent.js
+++ modules/excel/fun/initEvent.js
@@ -1,6 +1,11 @@
 // eslint-disable-next-line no-unused-vars
-import { dataObj, fbData, primaryKey, qTable, tableSelectKey, zbData, zbDataObj } from "../var/index"
-
+import { dataObj, fbData, primaryKey, qTable, tableSelectKey, zbData } from "../var/index"
+function setData(value, data) {
+  value.split(',').forEach(key => {
+    var v = JSON.parse(JSON.stringify(dataObj[key]))
+    data = data.concat(v)
+  })
+}
 export function initEvent() {
   layui.use(['form'], () => {
     layui.form.on('select(zb)', function ({ value }) {
@@ -10,22 +15,16 @@ export function initEvent() {
         data
       })
       zbData = []
-      zbDataObj = {}
-      value.split(',').forEach(key => {
-        var v = JSON.parse(JSON.stringify(dataObj[key]))
-        zbData = zbData.concat(v)
-        zbDataObj[key] = v
-      })
+      setData(value, zbData)
       qTable.tableReload(zbData);
     })
     layui.form.on('select(fjb)', function ({ value }) {
       fbData = []
-      value.split(',').forEach(key => {
-        fbData = fbData.concat(JSON.parse(JSON.stringify(dataObj[key])))
-      })
+      setData(value, fbData)
       setTimeout(() => {
         qTable.setHeight()
       }, 100);
+      console.log(fbData)
     })
     layui.form.on('select(col)', function ({ value }) {
       primaryKey = value.split(',')
