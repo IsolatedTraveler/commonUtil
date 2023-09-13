adb833f782b33c287699ea03fbf5e2b7ccf37eea
何波 <tbydmi@gmail.com>
2023-09-13 16:45:37 +0800

    Update index.js

--- layModules/jtEditTable/fun/init/index.js
+++ layModules/jtEditTable/fun/init/index.js
@@ -2,10 +2,8 @@ import { initEvent } from "./initEvent";
 import { initConfig } from "./initConfig";
 import { renderTable } from "../render/renderTable";
 import { initThird } from "./initThird";
-import { startRender } from "../other/changeIsInit";
 
 export function init(config) {
-  startRender('init')
   initThird()
   initConfig(config)
   renderTable().then(initEvent)
