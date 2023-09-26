a8973a0d08394c0a260c8cd7a20d78b11ebafb79
何波 <tbydmi@gmail.com>
2023-09-26 14:16:26 +0800

    Update export.js

--- src/commonUtil/public/fun/export.js
+++ src/commonUtil/public/fun/export.js
@@ -3,10 +3,28 @@ import { setIframe } from "./deeps"
 import { down } from "./down"
 import { loaded, loading, strToUrl, val, init, getParamsUrl } from "./init"
 import { dealsUrl } from "./url"
+function setHtml(title, body, head, { addBefore = '', addAfter = '' } = {}, index) {
+  down(strToUrl([
+    '<html xmlns:svg="http://www.w3.org/2000/svg"><head><meta name="content-type" content="text/html" charset="UTF-8"><title>',
+    title,
+    '</title></head><body>',
+    '<table><thead>',
+    addBefore,
+    head,
+    '</thead><tbody>',
+    body,
+    addAfter,
+    '</tbody></table></body>'
+  ].join(''), 'application/vnd.ms-excel'), title + '.xls')
+  loaded(index)
+}
+function getExportTr(it, cols) {
+  return `<tr>${cols.map(col => `<td style="mso-number-format:'\\@';">${it[col] || ''}</td>`).join('')}</tr>`
+}
 export function expExcel(param, type = '1') {
   let index = loading(), title = param.title || '导出'
   if (type === '1') {
-    let elem = $(param.elem), body = elem.find('tbody').eq(0).clone().html(), html
+    let elem = $(param.elem), body = elem.find('tbody').eq(0).clone().html()
     body = body.replace(/(<td ((?!style)[^>])*)(style[='"]+)*([^>]*)(>)/g, function (a, b, c, d, e, f) {
       if (d) {
         return b + d + "mso-number-format:'\\@';" + e + f
@@ -14,20 +32,9 @@ export function expExcel(param, type = '1') {
         return b + ' style="mso-number-format:\'\\@\';"' + f
       }
     })
-    html = [
-      '<html xmlns:svg="http://www.w3.org/2000/svg"><head><meta name="content-type" content="text/html" charset="UTF-8"><title>',
-      title,
-      '</title></head><body>',
-      '<table><thead>',
-      param.addBefore || '',
-      elem.find('thead').eq(0).html(),
-      '</thead><tbody>',
-      body,
-      param.addAfter || '',
-      '</tbody></table></body>'
-    ].join('')
-    down(strToUrl(html, 'application/vnd.ms-excel'), title + '.xls')
-    loaded(index)
+    setHtml(title, body, elem.find('thead').eq(0).html(), param, index)
+  } else if (type == '2') {
+    setHtml(title, (param.data || []).map(it => getExportTr(it, param.cols)).join(''), param.head, param, index)
   } else {
     let pro = Promise.resolve()
     if (!BASE64) {
