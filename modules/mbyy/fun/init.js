51477a22324498352df3986814dd8cb59fa6264f
何波 <tbydmi@gmail.com>
2023-10-18 15:20:54 +0800

    Update init.js

--- modules/mbyy/fun/init.js
+++ modules/mbyy/fun/init.js
@@ -1,5 +1,5 @@
 // eslint-disable-next-line no-unused-vars
-import { pageSize, pageElem } from "../var";
+import { pageSize, pageElem } from "../var/index";
 import { render } from "./init/render";
 
 export function init(config) {
