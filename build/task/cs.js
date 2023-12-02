525353b189f4cfbbbd3ce026566404c7c142aab9
何波 <tbydmi@gmail.com>
2023-12-02 16:44:40 +0800

    Update cs.js

--- build/task/cs.js
+++ build/task/cs.js
@@ -3,9 +3,10 @@ const { readDir, getCode } = require('../fun')
 const path = require('path'), ml = path.resolve(`${__dirname}/../../cs`)
   , { outMl } = require('../var/gModule')
 module.exports = function (grunt, version) {
-  return readDir(ml).then((res = []) => {
-    return Promise.all(res.map(name => {
-      return getCode(name, ml, version, grunt, outMl, 'cs')
-    }))
+  return readDir(ml).then(async (res = []) => {
+    let len = res.length
+    for (let i = 0; i < len; i++) {
+      await getCode(res[i], ml, version, grunt, outMl, 'cs')
+    }
   })
 }
