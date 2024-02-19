5d2900a8649370eca1ff0d649091e8fbffd8fe17
何波 <tbydmi@gmail.com>
2024-02-19 17:05:39 +0800

    Update paramget.ts

--- g-lobal/commonUtil/public/paramget.ts
+++ g-lobal/commonUtil/public/paramget.ts
@@ -1,16 +1,10 @@
 import { commonHttppost } from "../../ajax/public/post"
 import { systemDataParam } from "../../allVar"
-import { system } from "../../browser/var"
 import { setPageTemp } from "../../temp/tempData"
 
 export function paramget(mkdm: string, bh: string | number | undefined = undefined) {
-  if (system) {
-    let res = system.paramget(mkdm, bh)
-    return bh ? res : JSON.parse(res)
-  } else {
-    let res = setPageTemp(systemDataParam, paramSet, mkdm)
-    return bh ? res[bh] : res
-  }
+  let res = setPageTemp(systemDataParam, paramSet, mkdm)
+  return bh ? res[bh] : res
 }
 function paramSet(mkdm: string) {
   let data = commonHttppost('/xt01-xtjc/ty/s-xtcs', { mkdm, jqm: '' }).data || [], res: any = {}
