10b9e636a6a1c99d247640d156f5ee2f38408f3f
何波 <tbydmi@gmail.com>
2024-03-13 17:34:50 +0800

    Update cflx.ts

--- third/hlyy/erdss/fun/dic/cflx.ts
+++ third/hlyy/erdss/fun/dic/cflx.ts
@@ -1,7 +1,14 @@
 export type Cflx = 1 | 2 | 3 | 4
 export type IsLscf = 1 | 0
-export function dicCflx(): Cflx {
-  return 1
+export function dicCflx(yplx: string): Cflx {
+  if (yplx == '02') {
+    return 2
+  } else if (yplx == '01') {
+    return 1
+  } else if (yplx == '03') {
+    return 3
+  }
+  return 4
 }
 export function dicIsLscf(): IsLscf {
   return 1
