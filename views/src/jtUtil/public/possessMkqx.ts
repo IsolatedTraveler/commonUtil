dcc50fe4f6171b20261eb8c6dc8c2b94a0bf7381
何波 <tbydmi@gmail.com>
2024-04-20 21:45:34 +0800

    Update possessMkqx.ts

--- views/src/jtUtil/public/possessMkqx.ts
+++ views/src/jtUtil/public/possessMkqx.ts
@@ -1,8 +1,16 @@
 export function possessMkqx(mkbh: string, dm?: string) {
   try {
-    var res = GLOBAL$AJAX$.commonHttppost('', { mkbh, dm })
+    var res = GLOBAL$AJAX$.commonHttppost('/magic/xt01-xtjc/03/11/s-mkqx', { mkbh, dm })
     if (res.code === 1) {
-      return res.data.list
+      const list = res.data.list || []
+      if (dm) {
+        return ((list[0] || {}).mr || 0) + ''
+      } else {
+        return list.reduce(({ dm, mr }: any) => {
+          dm[dm] = mr + ''
+          return dm
+        }, {})
+      }
     } else {
       GLOBAL$LAYER$.alertMsg('获取权限失败:' + res.message)
     }
