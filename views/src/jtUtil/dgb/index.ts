c081caa6c12bb9c338a23ddeb502fb9f5ef5c428
何波 <tbydmi@gmail.com>
2024-04-20 21:54:07 +0800

    getCommonCombobox

--- views/src/jtUtil/dgb/index.ts
+++ views/src/jtUtil/dgb/index.ts
@@ -1,6 +1,7 @@
 export * from './dealAjaxData'
 export * from './filterComboboxData'
 export * from './filterDicData'
+export * from './getCommonCombobox'
 export * from './getCommonDic'
 export * from './initBaseBar'
 export * from './initDadaGrid_tab'
