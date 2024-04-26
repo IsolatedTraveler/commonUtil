e227020b2b491c597f4d340e6e0f0e0b972026c2
何波 <tbydmi@gmail.com>
2024-04-26 21:39:11 +0800

    Update index.ts

--- views/cs/gncs/business/index.ts
+++ views/cs/gncs/business/index.ts
@@ -3,3 +3,4 @@ Class.prototype.commonHttppost = GLOBAL$XHR$.commonHttppost
 Class.prototype.commonQueryAsyncHttppost_callback = GLOBAL$XHR$.commonQueryAsyncHttppost_callback
 Class.prototype.getAjax = GLOBAL$XHR$.getAjax
 Class.prototype.getAjaxAsync = GLOBAL$XHR$.getAjaxAsync
+Class.prototype.checkAuth = GLOBAL$AJAX$.magicCheckAuthV2
