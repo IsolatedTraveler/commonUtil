8989d4b45f80fc1028449e33e0d357c28719460a
何波 <tbydmi@gmail.com>
2024-04-26 14:58:33 +0800

    Update webs.ts

--- build/var/webs.ts
+++ build/var/webs.ts
@@ -1,5 +1,5 @@
 import path from 'path'
-import { ml as ml1 } from './public'
-export { date, out } from './public'
+import { ml as ml1, out } from './public'
+export { date } from './public'
 export const ml = path.resolve(ml1, 'webs')
-  , outMl = [path.resolve(ml1, '../distWeb')]
+  , outMl = out.map(it => path.resolve(it, '../../webs'))
