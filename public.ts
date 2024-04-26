fdf95c68dc6104487d8a681f01d99a7c219ce0db
何波 <tbydmi@gmail.com>
2024-04-26 13:33:44 +0800

    Update public.ts

--- public.ts
+++ public.ts
@@ -1,40 +1,7 @@
-export const buildModule = {
-  cs: {
-    dzpj: false,
-    hisAjax: false
-  },
-  src: {
-    jtSync: false,
-    commonUtil: false,
-    jtUtil: false
-  },
-  third: {
-    dzjkk: {
-      erdss: false
-    },
-    page: {
-      dz: false
-    },
-    hlyy: {
-      erdss: true
-    },
-    yprkgl: {
-      erdss: false
-    }
-  },
-  webs: {
-    his: {
-      ypgl: {
-        pdgl: {
-          pdjl: false
-        }
-      }
-    }
-  }
+import { InstitutionCode } from "./build/var/config"
+
+export const dbdq: InstitutionCode[] = ['erdss']
+export const dbnr = {
+  src: ['jtUtil'],
+  third: ['hlyy']
 }
-const dbdqObj = {
-  sc: 'E:/jtGit/web/his-flie/sc/lib23/js/',
-  nm: 'E:/jtGit/web/his-flie/nmhis/lib23/js/',
-  yq: 'E:/jtGit/web/his-flie/yq/lib23/js/'
-}
-export const dbdq = [dbdqObj.sc]
