99fb4fb1d73e42450a5c5d14a85d100d3221fe6d
何波 <tbydmi@gmail.com>
2024-04-26 16:24:50 +0800

    Update index.ts

--- views/cs/gncs/business/index.ts
+++ views/cs/gncs/business/index.ts
@@ -2,3 +2,7 @@ import { Class } from "../core";
 Class.prototype.commonHttppost = GLOBAL$AJAX$.commonHttppost
 Class.prototype.commonQueryAsyncHttppost_callback = GLOBAL$AJAX$.commonQueryAsyncHttppost_callback
 Class.prototype.getAjax = GLOBAL$AJAX$.getAjax
+Class.prototype.getAjaxSync = GLOBAL$AJAX$.getAjaxSync
+Class.prototype.getConfig = GLOBAL$AJAX$.getConfig
+Class.prototype.checkAuth = GLOBAL$AJAX$.magicCheckAuthV2
+Class.prototype.dealAjaxData = GLOBAL$AJAX$.dealAjaxData
