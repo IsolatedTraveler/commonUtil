4d648e954df18d69caa351b77cd4361ae560455e
何波 <tbydmi@gmail.com>
2024-04-26 16:53:48 +0800

    Update core.ts

--- views/cs/gncs/core.ts
+++ views/cs/gncs/core.ts
@@ -1,7 +1,5 @@
 export const Class: any = function (this: any) {
   that = this
-  GLOBAL$BROWSER$.getJtPhisSystem()
-  GLOBAL$COMMONUTIL$.setWebName()
   if (layui) {
     layui.use(['layer'])
   }
