9e2be03bcbffb1bb7cbca397866a7f1689a31e70
何波 <tbydmi@gmail.com>
2024-04-26 16:53:44 +0800

    Update index.ts

--- views/cs/gncs/business/index.ts
+++ views/cs/gncs/business/index.ts
@@ -5,4 +5,3 @@ Class.prototype.getAjax = GLOBAL$AJAX$.getAjax
 Class.prototype.getAjaxSync = GLOBAL$AJAX$.getAjaxSync
 Class.prototype.getConfig = GLOBAL$AJAX$.getConfig
 Class.prototype.checkAuth = GLOBAL$AJAX$.magicCheckAuthV2
-Class.prototype.dealAjaxData = GLOBAL$AJAX$.dealAjaxData
