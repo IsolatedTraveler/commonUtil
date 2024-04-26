694c6091bd4b75b86c9eb7e1465af525ef659ae8
何波 <tbydmi@gmail.com>
2024-04-26 20:38:45 +0800

    Update globalLocal.ts

--- types/globalLocal.ts
+++ types/globalLocal.ts
@@ -5,6 +5,6 @@ import {
   getRouterW, redirect, router, session, local, setPageTemp, tempData, getAllUrl, getUploadUrl, dealsUrl, getUrl, 
   getObjToUrl, getParamsUrl, getUrlParams, getBaseUrl, getMainUrl, getServiceUrl, strToUrl, dealMenu, exited, 
   getMenu, getUser, getUserInfo, login, exit, logOut, calc, debounce1, errFormat, formatTreeData, extractIDInfo, 
-  idCard, uuid, preciseDecimal, prefix, resFormat, getAge, commonQueryAsyncHttppost_callback, getAjaxSync, 
+  idCard, uuid, preciseDecimal, prefix, resFormat, getAge, commonQueryAsyncHttppost_callback, getAjaxAsync, 
   commonHttppost, getAjax, dealSheetToArray, expExcel, loadXlsx, readXlsx
 } from '../views/g-lobal'
