e0e96d86604d04eb57ae1be3f995752bc4877ba0
何波 <tbydmi@gmail.com>
2024-04-26 16:23:00 +0800

    Update globalLocal.ts

--- types/globalLocal.ts
+++ types/globalLocal.ts
@@ -1,12 +1,11 @@
 import {
   ajaxErrorCode, ajaxSuccessCode, setAjaxContentType, setAjaxSuccessCode, setJqMode, getAjax, getAjaxSync, 
   commonHttppost, commonQueryAsyncHttppost_callback, getConfig, upload, dealAjaxData, magicCheckAuth, 
-  magicCheckAuthV2, ajax, getAjaxRes, getJtPhisSystem, getSystemVal, bbPrint, getBrowserParam, 
-  setBrowserParam, errorTrace, openDialog, openMsgBox, skip, dealLogin, paramget, setWebName, getMd5, 
-  encryption, down, loadJs, loadJsJudge, loadStyle, readFile, uploadInputFile, alertMsg, confirmMsg, getLayui, 
-  load, loaded, loading, openPage, getName, getRouterW, redirect, router, session, local, setPageTemp, tempData, 
-  getAllUrl, getUploadUrl, dealsUrl, getUrl, getObjToUrl, getParamsUrl, getUrlParams, getBaseUrl, getMainUrl, 
-  getServiceUrl, strToUrl, dealMenu, exited, getMenu, getUser, getUserInfo, login, exit, logOut, calc, debounce1, 
-  errFormat, formatTreeData, extractIDInfo, idCard, uuid, preciseDecimal, prefix, resFormat, getAge, 
-  dealSheetToArray, expExcel, loadXlsx, readXlsx
+  magicCheckAuthV2, getJtPhisSystem, getSystemVal, bbPrint, getBrowserParam, setBrowserParam, errorTrace, 
+  openDialog, openMsgBox, skip, dealLogin, paramget, setWebName, getMd5, encryption, down, loadJs, loadJsJudge, 
+  loadStyle, readFile, uploadInputFile, alertMsg, confirmMsg, getLayui, load, loaded, loading, openPage, getName, 
+  getRouterW, redirect, router, session, local, setPageTemp, tempData, getAllUrl, getUploadUrl, dealsUrl, getUrl, 
+  getObjToUrl, getParamsUrl, getUrlParams, getBaseUrl, getMainUrl, getServiceUrl, strToUrl, dealMenu, exited, 
+  getMenu, getUser, getUserInfo, login, exit, logOut, calc, debounce1, errFormat, formatTreeData, extractIDInfo, 
+  idCard, uuid, preciseDecimal, prefix, resFormat, getAge, dealSheetToArray, expExcel, loadXlsx, readXlsx
 } from '../views/g-lobal'
