6f7f63afae2c63f57067df8b3babb536bdf8d612
何波 <tbydmi@gmail.com>
2024-04-26 15:49:41 +0800

    Update global.ts

--- build/task/global.ts
+++ build/task/global.ts
@@ -11,12 +11,11 @@ export function getFileUrl(url: string) {
 }
 export function taskGlobal(version: string) {
   return readDir(ml).then(async (res: Array<string> = []) => {
-    let len = res.length
-    for (let i = 0; i < len; i++) {
-      let naem = res[i], url = path.resolve(ml, naem, 'index.ts'), keys = Object.keys(require(url))
-      await createType(naem, keys)
-    }
-  }).then(async () => {
+    return Promise.all(res.map((name) => {
+      const url = path.resolve(ml, name, 'index.ts'), keys = Object.keys(require(url))
+      return createType(name, keys)
+    }))
+  }).then(() => {
     let url = path.resolve(ml, 'index.ts'), keys = Object.keys(require(url))
     return createIndex(keys)
   })
