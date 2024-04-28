b6b2d90fb975ca4948bba2d000bbafc8262d126a
何波 <tbydmi@gmail.com>
2024-04-28 13:35:46 +0800

    Create establishAppBaseUrl.ts

---
+++ views_V2024/g-lobal/url/public/establishAppBaseUrl.ts
@@ -0,0 +1,7 @@
+import { setPageTemp } from "../../../../views/g-lobal";
+import { appBaseUrl, setAppBaseUrl } from "../var";
+
+// 分析当前页面URL，确定应用的基础URL。
+export function establishAppBaseUrl() {
+  return setPageTemp(appBaseUrl, setAppBaseUrl)
+}
