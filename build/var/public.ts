3119ec9faae7677e1f7876dcf9d8ec8813b7e0b4
何波 <tbydmi@gmail.com>
2024-04-28 09:35:32 +0800

    Update public.ts

--- build/var/public.ts
+++ build/var/public.ts
@@ -1,6 +1,6 @@
 import path from 'path'
 import { institutionDeploymentMapping, deploymentDirectories } from './config'
-import { dbdq } from '../../public'
-export const ml = path.resolve(`${__dirname}/../../views/`)
+import { dbdq, dmSite } from '../../public'
+export const ml = path.resolve(`${__dirname}/../../${dmSite}/`)
   , out = [...new Set(dbdq.map(key => deploymentDirectories[institutionDeploymentMapping[key]]))]
   , date = new Date().toISOString().replace(/:\d+\.\d+Z$/, "Z")
