5dd9c2068f6933fadedf6e605a8c745fb5df47bb
何波 <tbydmi@gmail.com>
2024-04-28 13:37:30 +0800

    Update var.ts

--- views_V2024/g-lobal/url/var/var.ts
+++ views_V2024/g-lobal/url/var/var.ts
@@ -1,6 +1,9 @@
 import { urlPattern } from "./const"
 
-export var appBaseUrl: string
+export var appBaseUrl: string // 应用基础URL
+  , serverUrl: string // 服务端URL
+  , configData: string // 存储配置数据
+// 设置应用的基础URL
 export function setAppBaseUrl() {
   let url = window.location.href
   return appBaseUrl = (url.split(urlPattern)[0] + '/').replace(/\/+/g, '/')
