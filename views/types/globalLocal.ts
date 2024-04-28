81ba0232f6009a492e5b26d17b21b4ed5bfeb8f8
何波 <tbydmi@gmail.com>
2024-04-28 13:10:03 +0800

    Update globalLocal.ts

--- views/types/globalLocal.ts
+++ views/types/globalLocal.ts
@@ -1,11 +1,12 @@
 import {
-  getJtPhisSystem, getSystemVal, bbPrint, getBrowserParam, setBrowserParam, errorTrace, openDialog, 
-  openMsgBox, skip, dealLogin, paramget, setWebName, getMd5, encryption, down, loadJs, loadJsJudge, loadStyle, 
-  readFile, uploadInputFile, alertMsg, confirmMsg, getLayui, load, loaded, loading, openPage, getName, 
-  getRouterW, redirect, router, session, local, setPageTemp, tempData, getAllUrl, getUploadUrl, dealsUrl, getUrl, 
-  getObjToUrl, getParamsUrl, getUrlParams, getBaseUrl, getMainUrl, getServiceUrl, strToUrl, dealMenu, exited, 
-  getMenu, getUser, getUserInfo, login, exit, logOut, calc, debounce1, errFormat, formatTreeData, extractIDInfo, 
-  idCard, uuid, preciseDecimal, prefix, resFormat, getAge, magicCheckAuthV2, magicData2, 
-  commonQueryAsyncHttppost_callback, getAjaxAsync, commonHttppost, getAjax, dealSheetToArray, expExcel, 
-  loadXlsx, readXlsx
+  ajaxErrorCode, ajaxSuccessCode, setAjaxContentType, setAjaxSuccessCode, setJqMode, getAjax, getAjaxSync, 
+  commonHttppost, commonQueryAsyncHttppost_callback, getConfig, upload, dealAjaxData, magicCheckAuth, 
+  magicCheckAuthV2, magicData2, getJtPhisSystem, getSystemVal, bbPrint, getBrowserParam, setBrowserParam, 
+  errorTrace, openDialog, openMsgBox, skip, dealLogin, paramget, setWebName, getMd5, encryption, down, loadJs, 
+  loadJsJudge, loadStyle, readFile, uploadInputFile, alertMsg, confirmMsg, getLayui, load, loaded, loading, 
+  openPage, getName, getRouterW, redirect, router, session, local, setPageTemp, tempData, getAllUrl, 
+  getUploadUrl, dealsUrl, getUrl, getObjToUrl, getParamsUrl, getUrlParams, getBaseUrl, getMainUrl, 
+  getServiceUrl, strToUrl, dealMenu, exited, getMenu, getUser, getUserInfo, login, exit, logOut, calc, debounce1, 
+  errFormat, formatTreeData, extractIDInfo, idCard, uuid, preciseDecimal, prefix, resFormat, getAge, 
+  dealSheetToArray, expExcel, loadXlsx, readXlsx
 } from '../views/g-lobal'
