8f017d7c392babc9863a4c19eba95f50bf0a6dc3
何波 <tbydmi@gmail.com>
2024-04-28 13:38:06 +0800

    Create buildAbsoluteUrl.ts

---
+++ views_V2024/g-lobal/url/public/buildAbsoluteUrl.ts
@@ -0,0 +1,14 @@
+import { UrlType } from "../../../../types";
+import { convertToAbsoluteUrl } from "../fun";
+import { establishAppBaseUrl } from "./establishAppBaseUrl";
+import { obtainServiceEndpoint } from "./obtainServiceEndpoint";
+// 根据给定URL和类型，构建完整的绝对URL。
+export function buildAbsoluteUrl(targetUrl: string, urlType: UrlType) {
+  if (/^http/.test(targetUrl)) {
+    return targetUrl;
+  } else if (urlType === 'origin') {
+    return convertToAbsoluteUrl(targetUrl, establishAppBaseUrl());
+  } else {
+    return convertToAbsoluteUrl(targetUrl, obtainServiceEndpoint());
+  }
+}
