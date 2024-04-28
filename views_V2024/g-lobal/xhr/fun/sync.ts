4a50cf928d2cc9649351631f963178dcb820610e
何波 <tbydmi@gmail.com>
2024-04-28 17:09:20 +0800

    Create sync.ts

---
+++ views_V2024/g-lobal/xhr/fun/sync.ts
@@ -0,0 +1,28 @@
+import { AjaxRequestConfig, AjaxRequestOption } from "../../../../types";
+import { errFormat } from "../../util";
+import { ajaxTimeOut } from "../var";
+import { dealXhrRes } from "./deaXhrRes";
+import { setXhr } from "./setXhr";
+/**
+ * @description 发送同步请求
+ * @param {string} url - 请求地址
+ * @param {*} data - 请求数据
+ * @param {*} option - 请求选项
+ * @param {*} config - 配置信息
+ * @param {string} type - 请求方式
+ */
+export function sync(url: string, data: any = {}, param: any = {}, option: AjaxRequestOption = {}, config: AjaxRequestConfig = {}, type: 'GET' | 'POST') {
+  try {
+    const xhr = setXhr(url, type, option.urlType, param, config, false)
+    const time = setTimeout(() => {
+      xhr.abort()
+    }, ajaxTimeOut);
+    console.time()
+    xhr.send(data);
+    console.timeEnd()
+    clearTimeout(time)
+    return dealXhrRes(xhr)
+  } catch (e: any) {
+    return errFormat('请求过程中发生错误：' + (e.message || e))
+  }
+}
