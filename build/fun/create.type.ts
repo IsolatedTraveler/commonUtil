9445af4022921294f5875076a5ff3c5918adf87a
何波 <tbydmi@gmail.com>
2024-04-28 10:27:28 +0800

    Update create.type.ts

--- build/fun/create.type.ts
+++ build/fun/create.type.ts
@@ -1,22 +1,29 @@
+import { siteVesion } from '../../public'
+import { ml } from '../var/public'
 import { writeFile } from './createFile'
 import * as path from 'path'
-const ml = path.resolve(`${__dirname}/../../types/local`)
+const siteMl = path.resolve(ml, 'types/local')
+// const ml = path.resolve(`${__dirname}/../../types/local`)
+var add = ''
+if (siteVesion) {
+  add = '$' + siteVesion
+}
 export function createType(name: string, arr: Array<string>) {
-  let NAME = name.toUpperCase(), code = `import {
+  let NAME = name.toUpperCase(), varName = `GLOBAL$${NAME + add}$`, code = `import {
   ${arr.join(', ')}
 } from '../../views/g-lobal/${name}'
 declare global {
-  interface GLOBAL$${NAME}$TYPE {
+  interface ${varName}TYPE {
     ${arr.map(key => {
     return `${key}: typeof ${key}`
   }).join('\n    ')}
   }
-  let GLOBAL$${NAME}$: GLOBAL$${NAME}$TYPE
+  let ${varName}: ${varName}TYPE
 }
 export {
 
 }`
-  return writeFile(path.resolve(ml, `${name}.d.ts`), code)
+  return writeFile(path.resolve(siteMl, `${name}.d.ts`), code)
 }
 export function createIndex(arr: Array<string>) {
   let sum = 0, code = `import {
@@ -29,5 +36,5 @@ export function createIndex(arr: Array<string>) {
     return it
   }).join(', ')}
 } from '../views/g-lobal'`
-  return writeFile(path.resolve(ml, '../globalLocal.ts'), code)
+  return writeFile(path.resolve(siteMl, '../globalLocal.ts'), code)
 }
