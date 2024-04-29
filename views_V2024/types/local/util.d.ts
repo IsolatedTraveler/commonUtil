9b38217d365c64882c6383e0d796926287a6c28b
何波 <tbydmi@gmail.com>
2024-04-29 12:43:41 +0800

    Update util.d.ts

--- views_V2024/types/local/util.d.ts
+++ views_V2024/types/local/util.d.ts
@@ -1,9 +1,10 @@
 import {
-  errFormat, setPageTemp
+  errFormat, session, setPageTemp
 } from '../../views/g-lobal/util'
 declare global {
   interface GLOBAL$UTIL$V2024$TYPE {
     errFormat: typeof errFormat
+    session: typeof session
     setPageTemp: typeof setPageTemp
   }
   let GLOBAL$UTIL$V2024$: GLOBAL$UTIL$V2024$TYPE
