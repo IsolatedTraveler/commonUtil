a200854d5429bb1b102aaf88e07842063b2d8c35
何波 <tbydmi@gmail.com>
2024-04-29 13:08:16 +0800

    Update public.ts

--- public.ts
+++ public.ts
@@ -2,7 +2,6 @@ import { InstitutionCode } from "./build/var/config"
 interface Dbnr {
   [key: string]: string[]
 }
-export const dbdq: InstitutionCode[] = ['erdss']
 export const dbnr: Dbnr = {
   // cs: ['gncs'],
   // gModules: ['hisAjax'],
@@ -11,6 +10,7 @@ export const dbnr: Dbnr = {
   // third: ['hlyy'],
   // webs: ['his\\ypgl\\pdgl\\pdjl']
 }
+export const dbdq: InstitutionCode[] = ['smq']
 export const siteVesion = 'V2024'
 export const sfys = false
 export const qdzs = false
