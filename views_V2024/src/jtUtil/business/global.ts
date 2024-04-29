4de0af99b39b768d3aae47eeb65085070931fca9
何波 <tbydmi@gmail.com>
2024-04-29 15:12:13 +0800

    tb

---
+++ views_V2024/src/jtUtil/business/global.ts
@@ -0,0 +1,12 @@
+import { Class } from "../core";
+Class.prototype.commonHttppost = GLOBAL$AJAX$.commonHttppost
+Class.prototype.commonQueryAsyncHttppost_callback = GLOBAL$AJAX$.commonQueryAsyncHttppost_callback
+Class.prototype.getAjax = GLOBAL$AJAX$.getAjax
+Class.prototype.getAjaxSync = GLOBAL$AJAX$.getAjaxSync
+Class.prototype.getConfig = GLOBAL$AJAX$.getConfig
+Class.prototype.checkAuth = GLOBAL$AJAX$.magicCheckAuthV2
+Class.prototype.errorTrace = GLOBAL$BROWSER$.errorTrace
+Class.prototype.getUser = GLOBAL$USER$.getUser
+Class.prototype.openDialog = GLOBAL$BROWSER$.openDialog
+Class.prototype.openMsgBox = GLOBAL$BROWSER$.openMsgBox
+Class.prototype.getUrlParams = GLOBAL$URL$.getUrlParams
---
+++ views_V2024/src/jtUtil/business/index.ts
@@ -0,0 +1,4 @@
+import './dgb'
+import './global'
+import './public'
+import './var'
