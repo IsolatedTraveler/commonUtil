c1f4e124fad7bbb3454929860f721ab2ac397b66
何波 <tbydmi@gmail.com>
2024-04-29 10:27:16 +0800

    Update common.d.ts

--- views_V2024/types/local/common.d.ts
+++ views_V2024/types/local/common.d.ts
@@ -1,13 +1,9 @@
 import {
-  checkAuth, DEFAULT_AUTH_USER, XHR_JQ_CODE, XHR_JQ_URL, contentType, setServerUrl
+  checkAuth, setServerUrl
 } from '../../views/g-lobal/common'
 declare global {
   interface GLOBAL$COMMON$V2024$TYPE {
     checkAuth: typeof checkAuth
-    DEFAULT_AUTH_USER: typeof DEFAULT_AUTH_USER
-    XHR_JQ_CODE: typeof XHR_JQ_CODE
-    XHR_JQ_URL: typeof XHR_JQ_URL
-    contentType: typeof contentType
     setServerUrl: typeof setServerUrl
   }
   let GLOBAL$COMMON$V2024$: GLOBAL$COMMON$V2024$TYPE
