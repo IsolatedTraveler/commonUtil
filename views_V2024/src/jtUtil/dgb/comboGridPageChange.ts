4ebe8308354734185683947eb6b01c998c657265
hb <tbydmi@gmail.com>
2024-04-29 21:37:53 +0800

    Create comboGridPageChange.ts

---
+++ views_V2024/src/jtUtil/dgb/comboGridPageChange.ts
@@ -0,0 +1,33 @@
+import { loadComboGrigPageData } from "./loadComboGridPageData";
+/**
+ * 处理组合网格（Combogrid）的分页变化事件，包括页面切换、每页显示数量改变及刷新。
+ * @param {any} gridObject 组合网格对象的选择器或实例
+ * @param {string} url 数据加载的API地址
+ * @param {any} q 当前查询的关键字
+ * @param {any} param 其他请求参数对象，包含如blanksearch等状态标识
+ */
+export function comboGridPageChange(gridObject: any, url: any, q: any, param: any) {
+  try {
+    var pager = $(gridObject).combogrid('grid').datagrid('getPager');
+    const reloadData = (pageNumber: number, pageSize: number) => {
+      try {
+        if (q != '' || param.blanksearch) {
+          param.dm = q;
+          param.pageNumber = pageNumber;
+          param.pageSizeize = pageSize;
+          loadComboGrigPageData(gridObject, url, param);
+        }
+      } catch (e) {
+        GLOBAL$LAYER$V2024$.alertMsg(e);
+      }
+    }
+    $(pager).pagination({
+      displayMsg: '',
+      onRefresh: reloadData,
+      onChangePageSize:  (pageSize: number) => reloadData(1, pageSize),
+      onSelectPage: reloadData
+    });
+  } catch(e:any) {
+    GLOBAL$LAYER$V2024$.alertMsg(e.message || e)
+  }
+}
