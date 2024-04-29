117cc2eefc56fe01649b60a925ff97e2b5d7ca23
hb <tbydmi@gmail.com>
2024-04-29 21:04:38 +0800

    Create loadXzqhComboGrigPageData.ts

---
+++ views_V2024/src/jtUtil/dgb/loadXzqhComboGrigPageData.ts
@@ -0,0 +1,32 @@
+import { getXzqh } from "./getXzqh";
+/**
+ * @description 根据参数加载Combogrid的数据并处理分页
+ * @param {Object} gridObject Combogrid对象的jQuery引用
+ * @param {Object} param 加载数据所需的参数对象，应包含dm(查询条件), pageNumber(当前页码), pageSize(每页大小)
+ */
+
+export function loadXzqhComboGrigPageData(gridObject: any, param: any) {
+  try {
+    const grid = gridObject.combogrid("grid");
+    const pager = grid.datagrid('getPager');
+    // 同步Combogrid的分页器页码
+    if (param.pageNumber) {
+      if (param.pageNumber !== pager.pageNumber) {
+        pager.pagination({ pageNumber: param.pageNumber });
+      }
+    }
+    const query = param.dm;
+    if (query) {
+      const { list, total } =  getXzqh(query, param.pageNumber, param.pageSize);
+      if (total> 0) {
+        gridObject.combogrid('setValue', query);
+        grid.datagrid('loadData', {total, rows: list});
+        gridObject.combogrid('showPanel');
+      } else {
+        grid.datagrid('loadData',[])
+      }
+    }
+  } catch (e) {
+    GLOBAL$LAYER$V2024$.alertMsg(e);
+  }
+}
