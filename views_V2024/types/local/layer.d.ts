a52ced7b33760644268899089b4c3b9a328aec5a
何波 <tbydmi@gmail.com>
2024-04-29 13:58:15 +0800

    Create layer.d.ts

---
+++ views_V2024/types/local/layer.d.ts
@@ -0,0 +1,12 @@
+import {
+  alertMsg
+} from '../../g-lobal/layer'
+declare global {
+  interface GLOBAL$LAYER$V2024$TYPE {
+    alertMsg: typeof alertMsg
+  }
+  let GLOBAL$LAYER$V2024$: GLOBAL$LAYER$V2024$TYPE
+}
+export {
+
+}
