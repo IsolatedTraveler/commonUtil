e75f5b2f6cb130ed33cae9cb5ca9a00ab6af683d
何波 <tbydmi@gmail.com>
2024-04-29 10:26:51 +0800

    Update render.ts

--- views_V2024/cs/gncs/render.ts
+++ views_V2024/cs/gncs/render.ts
@@ -2,7 +2,7 @@
 import { Class } from "./core";
 // MODULE END
 import {
-  checkAuth, DEFAULT_AUTH_USER, XHR_JQ_CODE, XHR_JQ_URL, contentType, setServerUrl, getConfig, getUser,
+  checkAuth, setServerUrl, getConfig, getUser,
   buildAbsoluteUrl, getAppBaseUrl, convertObjectToQueryString, buildUrlWithQueryParams, getServerUrl,
   getAjax, asyncGetPost, asyncQueryPost, commonHttppost
 } from '../../g-lobal'
