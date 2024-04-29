34117bd187f584ba87ba050aef52c910413996ad
何波 <tbydmi@gmail.com>
2024-04-29 10:27:12 +0800

    Update globalLocal.ts

--- views_V2024/types/globalLocal.ts
+++ views_V2024/types/globalLocal.ts
@@ -1,5 +1,4 @@
 import {
-  checkAuth, setServerUrl, getConfig, getUser,
-  buildAbsoluteUrl, getAppBaseUrl, convertObjectToQueryString, buildUrlWithQueryParams, getServerUrl,
-  getAjax, asyncGetPost, asyncQueryPost, commonHttppost
+  checkAuth, setServerUrl, getConfig, getUser, buildAbsoluteUrl, getAppBaseUrl, convertObjectToQueryString, 
+  buildUrlWithQueryParams, getServerUrl, getAjax, asyncGetPost, asyncQueryPost, commonHttppost
 } from '../g-lobal'
