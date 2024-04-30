5f46f7d76d40099a7f387fc33929808f2feafe68
何波 <tbydmi@gmail.com>
2024-04-30 09:06:47 +0800

    Update layer.d.ts

--- views_V2024/types/local/layer.d.ts
+++ views_V2024/types/local/layer.d.ts
@@ -1,9 +1,10 @@
 import {
-  alertMsg, confirmMsg, openDialog, openMsgBox
+  alertMsg, closeParentPop, confirmMsg, openDialog, openMsgBox
 } from '../../g-lobal/layer'
 declare global {
   interface GLOBAL$LAYER$V2024$TYPE {
     alertMsg: typeof alertMsg
+    closeParentPop: typeof closeParentPop
     confirmMsg: typeof confirmMsg
     openDialog: typeof openDialog
     openMsgBox: typeof openMsgBox
