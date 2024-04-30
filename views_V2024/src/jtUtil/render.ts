6c0531781e1caf38666227e1ad43e0b96dbc00a0
何波 <tbydmi@gmail.com>
2024-04-30 10:35:25 +0800

    同步

--- views_V2024/src/jtUtil/render.ts
+++ views_V2024/src/jtUtil/render.ts
@@ -2,9 +2,10 @@
 import { Class } from "./core";
 // MODULE END
 import {
-  alertMsg, confirmMsg, openDialog, getConfig, getUser, checkAuth, setServerUrl, closeParentPop, openMsgBox,
-  buildAbsoluteUrl, getAppBaseUrl, convertObjectToQueryString, buildUrlWithQueryParams, getServerUrl,
-  getUrlParams, errFormat, session, setPageTemp, getAjax, asyncQueryGet, asyncQueryPost, commonHttppost
+  alertMsg, confirmMsg, openDialog, dicget, getConfig, getUser, possessMkqx, checkAuth, setServerUrl,
+  closeParentPop, openMsgBox, buildAbsoluteUrl, getAppBaseUrl, convertObjectToQueryString,
+  buildUrlWithQueryParams, getServerUrl, getUrlParams, errFormat, session, setPageTemp, getAjax,
+  asyncQueryGet, asyncQueryPost, commonHttppost
 } from '../../g-lobal'
 // @CODE
 // PLUGIN IGNORE START
