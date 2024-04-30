58cff484e94decdbd3aa022573fce1589288f8de
何波 <tbydmi@gmail.com>
2024-04-30 09:06:38 +0800

    Update global.ts

--- views_V2024/src/jtUtil/business/global.ts
+++ views_V2024/src/jtUtil/business/global.ts
@@ -1,12 +1,18 @@
 import { Class } from "../core";
+// xhr
 Class.prototype.commonHttppost = GLOBAL$XHR$V2024$.commonHttppost
 Class.prototype.commonQueryAsyncHttppost_callback = GLOBAL$XHR$V2024$.asyncQueryPost
 Class.prototype.getAjax = GLOBAL$XHR$V2024$.getAjax
 Class.prototype.getAjaxSync = GLOBAL$XHR$V2024$.asyncQueryGet
+// system
 Class.prototype.getConfig = GLOBAL$SYSTEM$V2024$.getConfig
+// common
 Class.prototype.checkAuth = GLOBAL$COMMON$V2024$.checkAuth
+// layer
 Class.prototype.alertMsg = GLOBAL$LAYER$V2024$.alertMsg
+Class.prototype.closeParentPop = GLOBAL$LAYER$V2024$.closeParentPop
 Class.prototype.getUser = GLOBAL$SYSTEM$V2024$.getUser
 Class.prototype.openDialog = GLOBAL$LAYER$V2024$.openDialog
 Class.prototype.openMsgBox = GLOBAL$LAYER$V2024$.openMsgBox
+// url
 Class.prototype.getUrlParams = GLOBAL$URL$V2024$.getUrlParams
