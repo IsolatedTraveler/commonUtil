c2aa0717e8e878ac0b3ab5e573ed46f57d8b8b12
何波 <tbydmi@gmail.com>
2024-04-30 10:07:03 +0800

    Update global.ts

--- views_V2024/src/jtUtil/business/global.ts
+++ views_V2024/src/jtUtil/business/global.ts
@@ -1,18 +1,19 @@
 import { Class } from "../core";
+// 确认加载
+// common
+Class.prototype.checkAuth = GLOBAL$COMMON$V2024$.checkAuth
+Class.prototype.getConfig = GLOBAL$COMMON$V2024$.getConfig
+Class.prototype.alertMsg = GLOBAL$COMMON$V2024$.alertMsg
+Class.prototype.getUser = GLOBAL$COMMON$V2024$.getUser
+Class.prototype.openDialog = GLOBAL$COMMON$V2024$.openDialog
+// layer
+Class.prototype.closeParentPop = GLOBAL$LAYER$V2024$.closeParentPop
+Class.prototype.openMsgBox = GLOBAL$LAYER$V2024$.openMsgBox
 // xhr
 Class.prototype.commonHttppost = GLOBAL$XHR$V2024$.commonHttppost
 Class.prototype.commonQueryAsyncHttppost_callback = GLOBAL$XHR$V2024$.asyncQueryPost
 Class.prototype.getAjax = GLOBAL$XHR$V2024$.getAjax
 Class.prototype.getAjaxSync = GLOBAL$XHR$V2024$.asyncQueryGet
-// system
-Class.prototype.getConfig = GLOBAL$SYSTEM$V2024$.getConfig
-// common
-Class.prototype.checkAuth = GLOBAL$COMMON$V2024$.checkAuth
-// layer
-Class.prototype.alertMsg = GLOBAL$LAYER$V2024$.alertMsg
-Class.prototype.closeParentPop = GLOBAL$LAYER$V2024$.closeParentPop
-Class.prototype.getUser = GLOBAL$SYSTEM$V2024$.getUser
-Class.prototype.openDialog = GLOBAL$LAYER$V2024$.openDialog
-Class.prototype.openMsgBox = GLOBAL$LAYER$V2024$.openMsgBox
 // url
 Class.prototype.getUrlParams = GLOBAL$URL$V2024$.getUrlParams
+// 未确认加载
