293514ee62616cbb78660495eb4d5e312ef079d7
何波 <tbydmi@gmail.com>
2024-05-14 11:19:52 +0800

    将数字转换为指定精度的十进制字符串表示形式。

--- views_V2024/g-lobal/util/public/toDecimalNumber.ts
+++ views_V2024/g-lobal/util/public/toDecimalNumber.ts
@@ -1,16 +1,13 @@
 /**
- * @description 将给定数值转换为指定精度的十进制数格式。
- * 如果输入为空，则默认为''。
- * 支持对精度的处理，并包含异常捕获确保程序稳定运行。
+ * @description 将数字转换为指定精度的十进制字符串表示形式。
  *
- * @param {any} num - 待转换的数值，可以是数字或字符串形式，空字符串将被视为0。
- * @param {any} precision - 要保留的小数位数，非数字或负数时将返回原始num值。
- * @returns {string} - 格式化后的十进制数字符串。
+ * @param {number} num - 待转换的数值。
+ * @param {PositiveInteger} [precision] - 期望的精度，即小数点后保留的位数。默认为undefined，此时不进行格式化处理。
+ * @returns {string} -  格式化后的字符串（如果指定了精度），或者原始数字（如果没有指定精度或输入非数字）。
  */
-export function toDecimalNumber(num: string | number, precision?: number | undefined): string {
-  num = num || ''
-  if (typeof precision === 'number' && precision >= 0 && !isNaN(num as any)) {
-    return parseFloat(num as string).toFixed(precision)
+export function toDecimalNumber(num: number, precision?: number): string | number {
+  if (precision && precision >= 0) {
+    return num.toFixed(precision)
   }
-  return String(num)
+  return num
 }
